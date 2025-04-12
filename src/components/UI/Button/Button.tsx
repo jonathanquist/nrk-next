300cc1267dffebc8cac68d1c73b57b0dc091413f
Jonathan Quist <jonathan.quist@outlook.com>
2025-04-12 18:37:07 +0200

    fixes

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -1,45 +1,45 @@
-import * as React from 'react';
-import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  'small inline-flex hover:underline items-center justify-center whitespace-nowrap rounded-md tracking text-base font-bold ring-offset-primary-300 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  "small inline-flex hover:underline items-center justify-center whitespace-nowrap rounded-md tracking text-base font-bold ring-offset-primary-300 transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: 'bg-accent-500 text-primary-100 hover:bg-accent-500/90',
+        default: "bg-accent-500 text-primary-100 hover:bg-accent-500/90",
         outline:
-          'border border-accent-500 text-primary-900 bg-primary-100 bg-opacity-0 hover:bg-opacity-30 ',
-        secondary: 'bg-primary-500 text-primary-100 hover:bg-primary-500/80',
-        ghost: 'hover:bg-primary-100 hover:text-primary-900',
-        link: 'font-fira text-primary-900 underline-offset-4 hover:underline',
+          "border border-accent-500 text-primary-900 bg-primary-100 bg-opacity-0 hover:bg-opacity-30 ",
+        secondary: "bg-primary-500 text-primary-100 hover:bg-primary-500/80",
+        ghost: "hover:bg-primary-100 hover:text-primary-900",
+        link: "font-fira text-primary-900 underline-offset-4 hover:underline",
       },
       size: {
-        default: 'rounded-lg text-lg px-8 py-3 gap-1.5 tracking-wider',
-        sm: 'rounded-md text-base h-8 px-4 pt-[2px]',
-        lg: 'rounded-xl text-xl px-12 py-4 gap-2.5 ',
-        snug: 'p-0 text-lg',
-        icon: '',
+        default: "rounded-lg text-lg px-8 py-3 gap-1.5 tracking-wider",
+        sm: "rounded-md text-base h-8 px-4 pt-[2px]",
+        lg: "rounded-xl text-xl px-12 py-4 gap-2.5 ",
+        snug: "p-0 text-lg",
+        icon: "",
       },
       hasIcon: {
-        left: '',
-        right: '',
-        none: '',
+        left: "",
+        right: "",
+        none: "",
       },
     },
     compoundVariants: [
-      { size: 'default', hasIcon: 'left', className: 'pl-5' },
-      { size: 'default', hasIcon: 'right', className: 'pr-5' },
-      { size: 'sm', hasIcon: 'left', className: 'pl-2' },
-      { size: 'sm', hasIcon: 'right', className: 'pr-2' },
-      { size: 'lg', hasIcon: 'left', className: 'pl-8' },
-      { size: 'lg', hasIcon: 'right', className: 'pr-8' },
+      { size: "default", hasIcon: "left", className: "pl-5" },
+      { size: "default", hasIcon: "right", className: "pr-5" },
+      { size: "sm", hasIcon: "left", className: "pl-2" },
+      { size: "sm", hasIcon: "right", className: "pr-2" },
+      { size: "lg", hasIcon: "left", className: "pl-8" },
+      { size: "lg", hasIcon: "right", className: "pr-8" },
     ],
     defaultVariants: {
-      variant: 'default',
-      size: 'default',
-      hasIcon: 'none',
+      variant: "default",
+      size: "default",
+      hasIcon: "none",
     },
   }
 );
@@ -50,19 +50,31 @@ export interface ButtonProps
   asChild?: boolean;
   Icon?: React.ElementType;
   iconClassName?: string;
-  iconPosition?: 'left' | 'right';
+  iconPosition?: "left" | "right";
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, children, ...props }, ref) => {
-    const { Icon, iconPosition = 'left', iconClassName } = props;
-    const Comp = 'button';
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      children,
+      Icon,
+      iconPosition = "left",
+      iconClassName,
+      ...props
+    },
+    ref
+  ) => {
+    const Comp = "button";
 
     const iconPaddingClass = Icon
-      ? iconPosition === 'left'
-        ? 'pl-4'
-        : 'pr-4'
-      : '';
+      ? iconPosition === "left"
+        ? "pl-4"
+        : "pr-4"
+      : "";
 
     return (
       <Comp
@@ -70,20 +82,20 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           buttonVariants({
             variant,
             size,
-            hasIcon: Icon ? iconPosition : 'none',
+            hasIcon: Icon ? iconPosition : "none",
             className,
           })
         )}
         ref={ref}
         {...props}
       >
-        {Icon && iconPosition === 'left' && <Icon className={iconClassName} />}
+        {Icon && iconPosition === "left" && <Icon className={iconClassName} />}
         {children}
-        {Icon && iconPosition === 'right' && <Icon className={iconClassName} />}
+        {Icon && iconPosition === "right" && <Icon className={iconClassName} />}
       </Comp>
     );
   }
 );
-Button.displayName = 'Button';
+Button.displayName = "Button";
 
 export { Button, buttonVariants };
