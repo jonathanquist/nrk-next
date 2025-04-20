54fdcf1e436f46b1f8160919da065b6dc4061f15
Jonathan Quist <jonathan.quist@outlook.com>
2025-04-20 14:12:48 +0200

    Schema fix

--- src/app/aktiviteter/schema/page.tsx
+++ src/app/aktiviteter/schema/page.tsx
@@ -1,21 +1,21 @@
-'use client';
+"use client";
 
-import { Breadcrumb } from '@/components/UI';
-import SinglePage from '@/components/SinglePage/SinglePage';
-import { usePage } from '@/hooks/useFetch';
-import { API } from '@/lib/const';
-import Daily from '@/components/Events/Schedule/ScheduleDaily';
-import ScheduleLarge from '@/components/Events/Schedule/ScheduleLarge';
-import ScheduleDaily from '@/components/Events/Schedule/ScheduleDaily';
+import { Breadcrumb } from "@/components/UI";
+import SinglePage from "@/components/SinglePage/SinglePage";
+import { usePage } from "@/hooks/useFetch";
+import { API } from "@/lib/const";
+import Daily from "@/components/Events/Schedule/ScheduleDaily";
+import ScheduleLarge from "@/components/Events/Schedule/ScheduleLarge";
+import ScheduleDaily from "@/components/Events/Schedule/ScheduleDaily";
 
 export default function Page() {
   const { data: page, isLoading } = usePage(API.SCHEMA);
 
   return (
     <>
-      <Breadcrumb section="Aktiviteter" current={'Schema'} />
-      {/* <SinglePage page={page} isLoading={isLoading} /> */}
-      <ScheduleDaily />
+      <Breadcrumb section="Aktiviteter" current={"Schema"} />
+      <SinglePage page={page} isLoading={isLoading} />
+      {/* <ScheduleDaily /> */}
     </>
   );
 }
